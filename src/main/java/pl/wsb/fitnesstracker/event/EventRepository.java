615778d4e485e386b6fd819d5e380779a05ef1d5
Lukasz Kubow <twoj-email@przyklad.com>
2026-05-10 15:07:35 +0200

    Lab 03 - Entities and Repositories Completed

---
+++ src/main/java/pl/wsb/fitnesstracker/event/EventRepository.java
@@ -0,0 +1,16 @@
+package pl.wsb.fitnesstracker.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT e FROM Event e WHERE e.startDate > :now ORDER BY e.startDate")
+    List<Event> findUpcoming(@Param("now") LocalDateTime now);
+}
